768d401c6547b0d6fab4a45bc78ce20cbe52d590
Bryan PHV <phanvanhobryan@gmail.com>
2026-06-06 00:26:22 +0200

    Update push-github.bat

--- push-github.bat
+++ push-github.bat
@@ -3,42 +3,17 @@ cd /d "%~dp0"
 
 echo.
 echo ========================================
-echo   Push vers GitHub - Portfolio Bryan PHV
+echo   Push public/ vers GitHub
 echo ========================================
 echo.
 
-:: Supprimer l'ancien .git si corrompu
-if exist ".git" (
-    echo Suppression ancien .git...
-    rmdir /s /q ".git"
-)
-
-:: Init git
-echo Initialisation Git...
-git init
-git checkout -b main
-
-:: Config identite
-git config user.email "phanvanhobryan@gmail.com"
-git config user.name "Bryan PHV"
-
-:: Ajouter tous les fichiers
-git add .
-
-:: Premier commit
-git commit -m "feat: init portfolio Vite + React"
-
-:: Connecter au repo GitHub
-git remote add origin https://github.com/phanvanhobryan-dev/portfolio-bryan-phv.git
-
-:: Push
-echo.
-echo Push vers GitHub...
-git push -u origin main --force
+git add public/
+git commit -m "feat: add public assets (images, favicon)"
+git push origin main
 
 echo.
 echo ========================================
-echo   DONE ! Va sur Vercel pour deployer.
+echo   DONE ! Vercel va se redeployer auto.
 echo ========================================
 echo.
 pause
